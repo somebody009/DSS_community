f5cfed004dc890c83b548b99a824046817551e6f
aadesh <aadeshbhujbal1@gmail.com>
2023-10-23 01:12:02 +0530

    Auth added

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <main className="flex min-h-screen w-full items-center justify-center">
+        {children}
+      </main>
+    </>
+  );
+}
