3f8d883988c1538ba4ce73c103f566427eb2419c
aadesh <aadeshbhujbal1@gmail.com>
2023-11-15 19:38:41 +0530

    Communitypage Done

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,10 +5,12 @@ import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+// import error from "next/error";
 
 export async function getUserById(params: any) {
   try {
@@ -86,3 +88,25 @@ export async function deleteUser(params: DeleteUserParams) {
     throw error;
   }
 }
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({}).sort({ createdAt: -1 });
+
+    return { users };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// export async function getAllUsers(params: GetAllUsersParams) {
+//   try {
+//     connectToDatabase();
+//   } catch (error) {}
+//   console.log(error);
+//   throw error;
+// }
