7549a22b04095ead38cf64e5e044555d5256888a
Aadesh Santosh Bhujbal <aadeshbhujbal1@gmail.com>
2024-01-21 20:31:39 +0530

    Global SearchAdded

--- components/shared/Navbar/navbar.tsx
+++ components/shared/Navbar/navbar.tsx
@@ -11,15 +11,15 @@ const Navbar = () => {
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
-          src="/assets/images/site-logo.svg"
-          width={23}
-          height={23}
-          alt="DevFlow"
+          src="/assets/images/abtech.svg"
+          width={125}
+          height={125}
+          alt="ABTECH"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev <span className="text-primary-500">Overflow</span>
-        </p>
+        {/* <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          AB <span className="text-primary-500">Tech</span>
+        </p> */}
       </Link>
 
       <GlobalSearch />
